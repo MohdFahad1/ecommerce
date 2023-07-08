47b2b80d5210c29d953da20f11a5a8f57eba2224
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-07-08 10:41:47 +0530

    Routes Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import SingleProduct from './pages/SingleProduct';
+import Error from './pages/Error';
 
 function App() {
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/products" element={<Products />}/>
+          <Route path="/cart" element={<Cart />}/>
+          <Route path="/singleproduct/:id" element={<SingleProduct />}/>
+          <Route path="*" element={<Error />}/>
+        </Routes>
+      </Router>
     </>
   );
 }
