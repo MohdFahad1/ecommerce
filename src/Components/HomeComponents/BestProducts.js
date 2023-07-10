aec2f1186c13c16b594bd9315100c07f8c28652a
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-07-10 16:14:42 +0530

    Best Products section Completed

--- src/Components/HomeComponents/BestProducts.js
+++ src/Components/HomeComponents/BestProducts.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { useProductContext } from '../../context/productContext';
+import { NavLink } from 'react-router-dom';
+
 
 const BestProducts = () => {
+  const { isLoading, featureProducts } = useProductContext();
+
+  console.log(featureProducts);
+
+  if (isLoading) {
+    return <div>Loding..........</div>
+  }
+
+  if (!featureProducts || featureProducts.length === 0) {
+    return <div>No products found.</div>;
+  }
+
+
+
   return (
-    <div>BestProducts</div>
+    <div className="bg-[#F7F8FB] py-5 flex flex-col items-center">
+      <div className="text-center mb-10">
+        <h3 className="text-[#a1a1a1] uppercase text-xxl font-medium">Check now</h3>
+        <h1 className="text-4xl font-semibold">Best Selling Products</h1>
+      </div>
+
+      {/* PRODUCTS LISTS */}
+      <div className="bg-[#F7F8FB] flex md:flex-row md:justify-around md:gap-20 py-5 flex-col w-[280px] gap-10 items-center justify-center">
+        {featureProducts.map((currElem) => {
+          return (
+            <NavLink to={`/singleproduct/${currElem.id}`} key={currElem.id}>
+              <div className="bg-black p-3 text-white">
+
+                <div class="relative w-[250px] h-[200px] overflow-hidden">
+                  <img src={currElem.image} alt={currElem.name} class="w-full h-full transition-transform duration-300 hover:scale-125 hover:opacity-50" />
+                </div>
+
+                <div className="flex justify-between text-lg font-medium mt-3">
+                  <p>{currElem.name}</p>
+                  <p>Rs. {currElem.price}</p>
+                </div>
+              </div>
+            </NavLink>
+          )
+        })}
+      </div>
+
+
+    </div>
   )
 }
 
--- src/context/productContext.js
+++ src/context/productContext.js
@@ -3,21 +3,50 @@
 // consumer => (for simplicity we use useContext Hook which is inside contextAPI)
 
 
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import axios from 'axios';
+import reducer from '../reducer/productReducer';
 
 const AppContext = createContext();
 
+
+const API = "https://api.pujakaitem.com/api/products";
+
+const initialState = {
+    isLoading: false,
+    isError: false,
+    products: [],
+    featuredProducts: [],
+}
 
 // The children inside the AppProvider is nothing but our App component which is wrapped inside the index.js
 
 // both the children below are same
 const AppProvider = ({ children }) => {
-    return <AppContext.Provider value={{myName: ""}}>{children}</AppContext.Provider>
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const getProducts = async (url) => {
+        dispatch({ type: "SET_LOADING" })
+        try {
+            const res = await axios.get(url);
+            const products = await res.data;
+            dispatch({ type: "SET_API_DATA", payload: products })
+        } catch (error) {
+            dispatch({ type: "API_ERROR" })
+        }
+    }
+
+    useEffect(() => {
+        getProducts(API);
+    }, [])
+    
+
+    return <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
 }
 
 
 // custom hook 
-
 const useProductContext = () => {
     return useContext(AppContext);
 }
